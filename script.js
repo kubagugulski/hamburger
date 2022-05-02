2c308ae83899725f0cc6fdfc59da48929f1cab92
Jakub Gugulski <jakub.gugulski1@gmail.com>
2022-05-02 20:15:40 +0200

    finished project

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const burger = document.querySelector('.burger')
+const bars = document.querySelector('.fa-bars')
+const close = document.querySelector('.fa-times')
+const nav = document.querySelector('nav ul')
+
+const menu = () => {
+    nav.classList.toggle('active')
+    burger.classList.toggle('active')
+    bars.classList.toggle('hide')
+    close.classList.toggle('hide')
+}
+
+burger.addEventListener('click', menu)
